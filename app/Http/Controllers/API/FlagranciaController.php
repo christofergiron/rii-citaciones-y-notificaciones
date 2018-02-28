b6dac6c48c8146b0a9bfda35726292ff5f88035c
Jimmy Figueroa <jimmyfigueroa@me.com>
2018-02-28 10:22:24 -0600

    fixed flagrancia controller bug

--- app/Http/Controllers/API/FlagranciaController.php
+++ app/Http/Controllers/API/FlagranciaController.php
@@ -12,9 +12,9 @@ use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use Validator;
-use App\Http\Requests\StoreDenunciaMP;
+// use App\Http\Requests\StoreDenunciaMP;
 
-class DenunciaMPController extends Controller
+class FlagranciaController extends Controller
 {
 
     public  $successStatus = 200;
@@ -29,105 +29,105 @@ class DenunciaMPController extends Controller
         $this->tools = new Tools($this->logger);
         $this->denuncia_mp_tools = new DenunciaMPTools;
         $this->root = false;
-        $this->StoreDenunciaMP = new StoreDenunciaMP;
+        // $this->StoreDenunciaMP = new StoreDenunciaMP;
     }
 
     public function index(Request $request){
-      # parsing
-      // $parsed_request = $this->tools->parse_request($request);
-      // $arr = $parsed_request[1];
-      $arr = $request->all();
-      // $id = $request->params->id
+      // # parsing
+      // // $parsed_request = $this->tools->parse_request($request);
+      // // $arr = $parsed_request[1];
+      // $arr = $request->all();
+      // // $id = $request->params->id
 
-       $validator = Validator::make($arr   , [
-         "token" => "required",
-       ]);
+      //  $validator = Validator::make($arr   , [
+      //    "token" => "required",
+      //  ]);
 
-       if ($validator->fails()) {
-         return response()->json(['error'=>'No Content due to null or empty parameters'], 403);
-       }
+      //  if ($validator->fails()) {
+      //    return response()->json(['error'=>'No Content due to null or empty parameters'], 403);
+      //  }
 
-       #get denuncia_mp
-       $res = $this->denuncia_mp_tools->mp_list_denuncias($arr["token"]);
+      //  #get denuncia_mp
+      //  $res = $this->denuncia_mp_tools->mp_list_denuncias($arr["token"]);
 
-       # chek for nulls
-       if (is_null($res)) {
-         return response()->json(['error'=>'No Content due to null or empty parameters'], 403);
-       }
+      //  # chek for nulls
+      //  if (is_null($res)) {
+      //    return response()->json(['error'=>'No Content due to null or empty parameters'], 403);
+      //  }
 
-       # return success response
-       return response()->json(['documentsTable'=>$res], $this->successStatus);
+      //  # return success response
+      //  return response()->json(['documentsTable'=>$res], $this->successStatus);
     }
 
     public function show(Request $request, $id) {
-      # parsing
-      // $parsed_request = $this->tools->parse_request($request);
-      // $arr = $parsed_request[1];
-      $arr = $request->all();
-      $this->log::alert($arr);
+      // # parsing
+      // // $parsed_request = $this->tools->parse_request($request);
+      // // $arr = $parsed_request[1];
+      // $arr = $request->all();
+      // $this->log::alert($arr);
 
 
-       $validator = Validator::make($arr, [
-         "token" => "required"
-       ]);
+      //  $validator = Validator::make($arr, [
+      //    "token" => "required"
+      //  ]);
 
-       if ($validator->fails()) {
-         return response()->json(['error'=>'No Content due to null or empty parameters'], 403);
-       }
+      //  if ($validator->fails()) {
+      //    return response()->json(['error'=>'No Content due to null or empty parameters'], 403);
+      //  }
 
-       #get denuncia_mp
-       $res = $this->denuncia_mp_tools->mp_denuncia($id, $arr["token"]);
+      //  #get denuncia_mp
+      //  $res = $this->denuncia_mp_tools->mp_denuncia($id, $arr["token"]);
 
-       # chek for nulls
-       if (is_null($res)) {
-         return response()->json(['error'=>'No Content due denuncia mp is invalid!'], 403);
-       }
+      //  # chek for nulls
+      //  if (is_null($res)) {
+      //    return response()->json(['error'=>'No Content due denuncia mp is invalid!'], 403);
+      //  }
 
-       # return success response
-       return response()->json(['success' => true, 'message'=>json_decode($res)], $this->successStatus);
+      //  # return success response
+      //  return response()->json(['success' => true, 'message'=>json_decode($res)], $this->successStatus);
     }
 
     public function store(Request $request) { //StoreDenunciaMP $request
-      # parsing 
-      // $parsed_request = $this->tools->parse_request($request);
-      // $arr = $parsed_request[1];
-      $arr = $request->all();
-      $this->logger->alert('inside Store DenunciaMP');
-      $this->logger->alert(json_encode($arr)) ;   
-      $res = $this->StoreDenunciaMP->rules($arr);
+      // # parsing 
+      // // $parsed_request = $this->tools->parse_request($request);
+      // // $arr = $parsed_request[1];
+      // $arr = $request->all();
+      // $this->logger->alert('inside Store DenunciaMP');
+      // $this->logger->alert(json_encode($arr)) ;   
+      // $res = $this->StoreDenunciaMP->rules($arr);
 
-      if ($res->code != 200) {
-        return response()->json(['error'=>$res->message], 403);
-      }
+      // if ($res->code != 200) {
+      //   return response()->json(['error'=>$res->message], 403);
+      // }
 
-      $res = $this->StoreDenunciaMP->persist($arr);
+      // $res = $this->StoreDenunciaMP->persist($arr);
 
-      if (!$res->success) {
-        return response()->json(['error'=>$res->message], 403);
-      }
-       # return success response
-       return response()->json(['success' => $res->success, 'message'=>$res->message, 'payload' => $res->payload], $this->successStatus);
+      // if (!$res->success) {
+      //   return response()->json(['error'=>$res->message], 403);
+      // }
+      //  # return success response
+      //  return response()->json(['success' => $res->success, 'message'=>$res->message, 'payload' => $res->payload], $this->successStatus);
     }
 
     public function workflow(Request $request) {
-      $arr = $request->all();
-      $this->logger->alert('inside workflow service DenunciaMP');
-      $this->logger->alert(json_encode($arr)) ;   
-      $res = $this->StoreDenunciaMP->workflow_rules($arr);
+      // $arr = $request->all();
+      // $this->logger->alert('inside workflow service DenunciaMP');
+      // $this->logger->alert(json_encode($arr)) ;   
+      // $res = $this->StoreDenunciaMP->workflow_rules($arr);
 
-      if ($res->code != 200) {
-        return response()->json(['error'=>$res->message], 403);
-      }
+      // if ($res->code != 200) {
+      //   return response()->json(['error'=>$res->message], 403);
+      // }
 
-      //$res = $this->StoreDenunciaMP->asignar_delitos($arr);
-      $res = $this->StoreDenunciaMP->apply_transition($arr);      
+      // //$res = $this->StoreDenunciaMP->asignar_delitos($arr);
+      // $res = $this->StoreDenunciaMP->apply_transition($arr);      
 
-      if ($res->code != 200) {
-        return response()->json(['error'=>$res->message], 403);
-      } 
+      // if ($res->code != 200) {
+      //   return response()->json(['error'=>$res->message], 403);
+      // } 
 
-       # return success response
-       return response()->json(['success' => true, 'message'=>$res], $this->successStatus);      
+      //  # return success response
+      //  return response()->json(['success' => true, 'message'=>$res], $this->successStatus);      
     }
 
 }
